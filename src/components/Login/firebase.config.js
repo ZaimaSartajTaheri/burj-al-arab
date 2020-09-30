1cec015f244fcbb81e428c17f3d24c7df8db1eb2
ZaimaSartajTaheri <67533852+ZaimaSartajTaheri@users.noreply.github.com>
2020-10-01 02:39:45 +0600

    first commit

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDXiYUMyY5yWVveyi5TlxRQtzhreDZApLk",
+  authDomain: "burj-al-arab-14121.firebaseapp.com",
+  databaseURL: "https://burj-al-arab-14121.firebaseio.com",
+  projectId: "burj-al-arab-14121",
+  storageBucket: "burj-al-arab-14121.appspot.com",
+  messagingSenderId: "469736174080",
+  appId: "1:469736174080:web:e388612c3e7724719d9ee5"
+};
+export default firebaseConfig;
